99a6fdcef04a796cc53b6a766973d5c9ae2d0c22
Paulo Henrique <phpessoal2021@gmail.com>
2025-05-05 21:19:36 -0300

    feat: primeiro commit

---
+++ src/main/java/br/com/fiap/voluta/VolutaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.voluta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolutaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolutaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/voluta/VolutaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.voluta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolutaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
